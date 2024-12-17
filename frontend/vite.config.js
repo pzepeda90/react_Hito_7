218cb97345baf01d7b4ea3db502d9af67ea6e68a
Patricio Zepeda <patriciozepeda@MacBook-Pro.local>
2024-12-16 21:12:24 -0300

    intento 7

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Swal from 'sweetalert2'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['sweetalert2']
+      external: ['sweetalert2', 'sweetalert2/dist/sweetalert2.js'],
+      output: {
+        globals: {
+          'sweetalert2': 'Swal',
+          'sweetalert2/dist/sweetalert2.js': 'Swal'
+        }
+      }
     }
   },
-  resolve: {
-    alias: {
-      'sweetalert2': 'sweetalert2/dist/sweetalert2.js'
-    }
+  optimizeDeps: {
+    include: ['sweetalert2']
   }
 })
